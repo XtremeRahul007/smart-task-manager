97b6fc9df2ac16def4f3a91157d9b286f4826a2b
XtremeRahul007 <191006799+XtremeRahul007@users.noreply.github.com>
2026-06-12 20:53:02 +0530

    Delete src directory

--- src/ts/services/loaderService.ts
+++
@@ -1,5 +0,0 @@
-export function hideLoader() {
-    const hideLoader = document.querySelector(".loader-container");
-    hideLoader?.classList.add("fade-out");
-    setTimeout(() => hideLoader?.remove(), 300);
-}
--- src/ts/utils/formatText.ts
+++
@@ -1,4 +0,0 @@
-export function truncateText(text: string, maxLength: number) {
-    if (!text) return;
-    return text.length > maxLength ? text.slice(0, maxLength) + "...": text; 
-}
